393c5ab9a4a3a4136bedda1bf2e2a670671ec1cb
sagarbhure <sagarbhureaerospace@gmail.com>
2023-01-25 11:36:36 +0000

    fix typo

--- update_feeds.sh
+++ update_feeds.sh
@@ -3,4 +3,4 @@
 outdir="ip_feeds"
 curl -vL "https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=1.1.1.1" > "$outdir/tor_exit_nodes.txt"
 curl -vL "http://www.talosintelligence.com/documents/ip-blacklist" > "$outdir/talos_blacklist.txt"
-curl -VL "https://myip.ms/files/bots/live_webcrawlers.txt" > "$outdir/web_crawler.txt
+curl -VL "https://myip.ms/files/bots/live_webcrawlers.txt" > "$outdir/web_crawler.txt"
